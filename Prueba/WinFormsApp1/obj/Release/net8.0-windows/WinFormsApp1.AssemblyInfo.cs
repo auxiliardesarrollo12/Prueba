5a877b4412668b134c57b08d25eb9fb7b698a4fa
auxiliardesarrollo12 <jalil.benitez@agrisem.mx>
2026-03-18 09:44:54 -0700

    Actualizacion 3

--- Prueba/WinFormsApp1/obj/Release/net8.0-windows/WinFormsApp1.AssemblyInfo.cs
+++ Prueba/WinFormsApp1/obj/Release/net8.0-windows/WinFormsApp1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WinFormsApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+72f332babdf135db0f935bf75447f88a0a2d2ce5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9dade7b3ad4f145b9b13d4acdceeed973eac3e57")]
 [assembly: System.Reflection.AssemblyProductAttribute("WinFormsApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WinFormsApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
